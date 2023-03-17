23a728cd0dabf63264e2515a231866e9815674ac
Pranshu Singhal <singhalpranshu12@gmail.com>
2023-03-17 18:27:37 +0530

    vet project, used base template in Django to remove code repeatition

---
+++ djangovet/vetoffice/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+  context = {"name": "Spot"}
+  return render(request, "vetoffice/home.html", context)
