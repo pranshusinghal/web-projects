2f19a2c753705bbf1507cb2b2ad20fa887111780
Pranshu Singhal <singhalpranshu12@gmail.com>
2023-03-19 20:51:43 +0530

    CRUD functionality added Full-Stack

--- djangovet/djangovet/urls.py
+++ djangovet/djangovet/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
  
 urlpatterns = [
   path("admin/", admin.site.urls),
-  path("", include("vetoffice.urls")),
+  path("vetoffice/", include("vetoffice.urls")),
 ]
--- djangovet/vetoffice/urls.py
+++ djangovet/vetoffice/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
+
 from . import views
- 
+
+# Add your path()s inside of urlpatterns:
 urlpatterns = [
-  path('', views.home, name="home"),
+  path("", views.home, name="home"),
+  path("owner/list", views.OwnerList.as_view(), name="ownerlist"),
+  path("patient/list", views.PatientList.as_view(), name="patientlist"),
+  path("owner/create", views.OwnerCreate.as_view(), name="ownercreate"),
+  path("patient/create", views.PatientCreate.as_view(), name="patientcreate"),
+  path("owner/update/<pk>", views.OwnerUpdate.as_view(), name="ownerupdate"),
+  path("patient/update/<pk>", views.PatientUpdate.as_view(), name="patientupdate"),
+  path("owner/delete/<pk>", views.OwnerDelete.as_view(), name="ownerdelete"),
+  path("patient/delete/<pk>", views.PatientDelete.as_view(), name="patientdelete"),
 ]
